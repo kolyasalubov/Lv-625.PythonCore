06b4ea6c407a5402b57b091debc49f93949540d2
Gena Shpak <gy.shpak@gmail.com>
2021-08-27 22:35:43 +0300

    Changes to be committed: 	modified:   Hw4/gyshpak/Task2.py

--- Hw4/gyshpak/Task2.py
+++ Hw4/gyshpak/Task2.py
@@ -1,4 +1,12 @@
-our_list = (35,57,81,93,34,75,23,7,12)
+our_list = [35,57,81,93,]
+
 for i in our_list:
-    tmp_number = float(i)
-    our_list[n] = tmp_number
+    print(f"Type of data befor change is {type(i)}")
+
+n = 0
+for i in our_list:
+    our_list[n] = float(i)
+    n += 1
+
+for i in our_list:
+    print(f"Type of data after change is {type(i)}")
