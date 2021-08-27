91ede731f6eecdbf3a7f162278ce99fa696dc1c9
grantigor <grant.igor.v@gmail.com>
2021-08-27 21:39:31 +0300

    On branch Grant_Igor Changes to be committed: modified:   HW1_TASK2.py

--- HW1/Grant_Igor/HW1_TASK2.py
+++ HW1/Grant_Igor/HW1_TASK2.py
@@ -1,8 +1,8 @@
 number_one = n1 = int(input("Enter first number: "))
 number_two = n2 = int(input("Enter second number: "))
-print("{}+{}={}".format(n1, n2, n1+n2))
-print("{}-{}={}".format(n1, n2, n1-n2))
+print("%s+%s=%s" %(n1, n2, n1+n2))
+print("%d-%d=%d" %(n1, n2, n1-n2))
 print("{}/{}={}".format(n1, n2, n1/n2))
-print("{}*{}={}".format(n1, n2, n1*n2))
-print("{}**{}={}".format(n1, n2, n1**n2))
+print("{x}*{y}={z}".format(x=n1, y=n2, z=n1*n2))
+print(f"{n1}**{n2}={n1**n2}")
 
