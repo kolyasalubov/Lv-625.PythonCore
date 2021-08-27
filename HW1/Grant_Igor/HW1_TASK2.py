b1013cbb3ec0389fbfe4a31443f98785d2698832
grantigor <grant.igor.v@gmail.com>
2021-08-28 00:41:22 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   .gitignore 	modified:   HW1/Grant_Igor/HW1_TASK2.py 	modified:   HW3/Grant_Igor/HW3,TASK1 .py 	modified:   HW3/Grant_Igor/HW3,TASK3.py 	new file:   HW3/Grant_Igor/Zen.txt

--- HW1/Grant_Igor/HW1_TASK2.py
+++ HW1/Grant_Igor/HW1_TASK2.py
@@ -1,8 +1,7 @@
 number_one = n1 = int(input("Enter first number: "))
 number_two = n2 = int(input("Enter second number: "))
-print("%s+%s=%s" %(n1, n2, n1+n2))
+print("%s+%s=%s" %( n1, n2, n1+n2 ))
 print("%d-%d=%d" %(n1, n2, n1-n2))
 print("{}/{}={}".format(n1, n2, n1/n2))
 print("{x}*{y}={z}".format(x=n1, y=n2, z=n1*n2))
 print(f"{n1}**{n2}={n1**n2}")
-
