e3a1b643bba1a6ca8872f1ad47c4d8465cbc9a17
Andriy Dzhala <andrijdzhala42@gmail.com>
2021-08-29 15:33:20 +0300

    redacted home work1 Task1

--- HW1/Dzhand/first.py
+++ HW1/Dzhand/first.py
@@ -1,6 +1,11 @@
-a = 6
-b = 10
-if a > b:
-    print("a>b")
-else:
-    print("a<b")
+a = 24
+b = 42
+c = 2
+
+print ("a+b", a+b)
+print ("a-b", a-b)
+print ("b/a", b/a)
+print ("ac", a**c)
+print ("a*b", a*b)
+print ("a+bc-ac", a+b**c-a**c)
+print ("b/a+(a+b)-(c+b+a)", b/a+(a+b)-(c+b+a))
