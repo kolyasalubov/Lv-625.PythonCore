00ee25da46b618ee14cb056012fbc18f933dea69
grantigor <grant.igor.v@gmail.com>
2021-08-30 12:26:16 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   HW3/Grant_Igor/HW3,TASK2.py 	renamed:    HW4/Grant_Igor/none.txt -> HW4/Grant_Igor/HW4_TASK1.PY 	new file:   HW4/Grant_Igor/HW4_TASK2.PY 	new file:   HW4/Grant_Igor/HW4_TASK3.PY

---
+++ HW3/Grant_Igor/HW3,TASK2.py
@@ -0,0 +1,13 @@
+number = input("Enter your number: ")
+number = str(number)
+print("Reversed number is : ", number[::-1])
+sum = 0
+for n in number:
+    sum += int(n)
+print("Sum of all numbers is: ", sum)
+list = [i for i in number]
+sorted_list = sorted(list)
+print("Your sorted list is: ", sorted_list)
+
+
+
