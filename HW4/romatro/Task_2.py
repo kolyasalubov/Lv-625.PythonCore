de7a0a16190ad1099feeecdeb168e5eabc4c6178
romatro <roman.troyanovskyy@gmail.com>
2021-08-31 18:06:47 +0300

    adding task + correcting issue

--- HW4/romatro/Task_2.py
+++ HW4/romatro/Task_2.py
@@ -1,5 +1,4 @@
-last_range_item = input("please input positibe number to define list items range: ")
-list_to_transform = list(range(100))
+last_range_item = int(input("please input positibe number to define list items range: "))
+list_to_transform = list(range(last_range_item))
 for element in list_to_transform:
     element = float(element)
-    print (type(element))
