89af6e62fd9349017db173f6234b517e746f1813
KitOlga <kit.olga81@gmail.com>
2021-08-31 17:06:54 +0300

    Added hometask 4

--- HW4/KitO/Task1.py
+++ HW4/KitO/Task1.py
@@ -2,5 +2,11 @@
 
 customer_number = int(input('Enter your number: '))
 print(f'Your number is: {customer_number}')
+factorial = 1
 
-
+if customer_number in (0,1):
+    factorial = 1
+else:
+    for n in range(2,customer_number+1):
+        factorial = factorial * n
+print(f'The factorial of the entered number is {factorial}')
