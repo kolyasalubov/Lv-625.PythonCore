2a18b82780305c1c5ed773dad339832f3aad5a0c
KitOlga <kit.olga81@gmail.com>
2021-08-31 08:35:31 +0300

    Added hometask 1

---
+++ HW1/KitO/2.py
@@ -0,0 +1,11 @@
+# Define variables a and b. Read values a and b from console and calculate: 
+# a + b, a - b, a * b, a / b, a**b. Output obtained results.
+
+a = 23
+b = 5
+
+print(f'a + b = {a} + {b} = {a + b}')
+print(f'a - b = {a} - {b} = {a - b}')
+print(f'a * b = {a} * {b} = {a * b}')
+print(f'a / b = {a} / {b} = {a / b}')
+print(f'a ** b = {a} ** {b} = {a ** b}')
