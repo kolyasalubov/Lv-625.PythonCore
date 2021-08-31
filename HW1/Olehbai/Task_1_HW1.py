d5c11b849abebaa1dc42b125780608e8a4a14e7f
Oleh Horbai <oleg.gorbay.02@gmail.com>
2021-08-31 12:30:17 +0300

    added task1 & task2 from home work 1

---
+++ HW1/Olehbai/Task_1_HW1.py
@@ -0,0 +1,6 @@
+user_name = input("What is your name?\n")
+print("Hello,", user_name)
+user_age = input("How old are you?\n")
+print("Your age is ", user_age)
+user_loc = input("Where do you live?\n")
+print("Your live in ", user_loc)
---
+++ HW1/Olehbai/Task_2_HW1.py
@@ -0,0 +1,7 @@
+a = 69
+b = 21
+print("a+b =", a+b)
+print("a-b =", a-b)
+print("a*b =", a*b)
+print("a/b =", a/b)
+print("a**b =", a**b)
