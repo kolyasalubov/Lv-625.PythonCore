d23eb166b29de56e7aeb78566d9d954dd10303f8
Yura Antoniv <yraantoniv7@gmail.com>
2021-09-01 14:35:40 +0300

    added hw1

---
+++ HW1/yantoniv/Task1.py
@@ -0,0 +1,7 @@
+user_name = input("What is your name? ")
+user_age = input("How old are you? ")
+user_city = input("Where do you live? ")
+
+print(f'Hello, {user_name}')
+print(f'Your age is {user_age}')
+print(f'You live in {user_city}')
---
+++ HW1/yantoniv/Task2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the number a: "))
+b = int(input("Enter the number b: "))
+
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a / b)
+print('a ** b =', a ** b)
+
