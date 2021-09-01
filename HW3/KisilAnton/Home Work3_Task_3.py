0ee50d3add00b2ccd5930d305ab51640f78b046b
Anton Kisil <kisilanton1988@gmail.com>
2021-09-01 15:26:04 +0300

    added HW3_Task3

--- HW3/KisilAnton/Home Work3_Task_3.py
+++ HW3/KisilAnton/Home Work3_Task_3.py
@@ -1,4 +1,5 @@
 variable_1=float(input('Enter 1 variable: '))
 variable_2=float(input('Enter 2 variable: '))
-print('variable 1 =',variable_1+variable_2-variable_1)
-print('variable 2 =',variable_1+variable_2-variable_2)
+variable_1,variable_2=variable_2,variable_1
+print('variable 1 =',variable_1)
+print('variable 2 =',variable_2)
