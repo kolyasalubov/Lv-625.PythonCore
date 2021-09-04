5cb7d3390f2721ba92c14e084bbd59aa5d81a2e3
grantigor <grant.igor.v@gmail.com>
2021-09-04 22:55:04 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   HW5_task1.py

--- HW5/Grant_Igor/HW5_task1.py
+++ HW5/Grant_Igor/HW5_task1.py
@@ -1,2 +1,14 @@
-number_divided_by_two = [i for i in range(10) if i % 3 == 1]
-print(number_divided_by_two)
+# Numbers in range of 10 divisible by 2
+for i in range(10):
+    if i % 2 == 0:
+        print(i, end=" ")
+
+# Numbers in range of 10 divisible by 3
+for i in range(10):
+    if i % 3 == 0:
+        print(i, end=" ")
+
+# Numbers in range of 10 that are not divisible by 2 nor 3
+for i in range(10):
+    if i % 2 != 0 and i % 3 != 0:
+        print(i, end=" ")
