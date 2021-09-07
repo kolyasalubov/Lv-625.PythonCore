3605bde97c64c9e41196f4860f2d7ca8d068f5b1
KitOlga <kit.olga81@gmail.com>
2021-09-07 14:08:48 +0300

    Added HW4 Task 2

--- HW4/KitO/Task2.py
+++ HW4/KitO/Task2.py
@@ -2,13 +2,10 @@
 # Create a list that contains elements of integer type, 
 # then use the loop to change the type of these elements to a floating type.
 
-customer_number = int(input('Enter your number: '))
-print(f'Your number is: {customer_number}')
-list_int = list(range(0,customer_number,2))
-print(list_int)
-
+customer_number = int(input("Enter your number under 100: "))
+list_int = list(range(0,customer_number,1))
 n = 0
 for n in list_int:
-    list_float[n] = float(list_int[n])
+    list_int[n] = float(list_int[n])
     n += 1
-print(list_float)
+print(list_int)
