54715b231b6e979eac31b76b5ed4fd45a53b568c
Gena Shpak <gy.shpak@gmail.com>
2021-09-08 23:57:06 +0300

    modified:   Hw6/gyshpak/calculates the number of characters(Task3).py modified functinon for returned numbers of characters from string.

--- Hw6/gyshpak/calculates the number of characters(Task3).py
+++ Hw6/gyshpak/calculates the number of characters(Task3).py
@@ -1,8 +1,8 @@
-def numb_characters_str(arg):
+def numb_characters_on_str(arg : str) -> list:
     """
     returned calculate the number of characters included in a given string 
     """
-    return(len(arg))
+    return(len(arg),{x1:arg.count(x1)  for x1 in arg})
 
-our_len = numb_characters_str(input("enter string "))
-print(f"in your string is {our_len} characters")
+our_len = numb_characters_on_str(input("enter string "))
+print("in your string is {} characters:\n{}".format(our_len[0],our_len[1]))
