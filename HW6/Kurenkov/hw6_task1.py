541af22b75c17e6552f1147f1d90ecbf61e8dd82
Alexey Kurenkov <0959471995ak@gmail.com>
2021-09-08 12:25:43 +0300

    Added HW6

---
+++ HW6/Kurenkov/hw6_task1.py
@@ -0,0 +1,5 @@
+# Write a function that returns the largest number of two numbers
+# (use DocStrings documentation strings in the function).
+
+def max_of_two(*args):
+    return max(args)
---
+++ HW6/Kurenkov/hw6_task2.py
@@ -0,0 +1,13 @@
+# Write a program that calculates the square of ​​a rectangle, triangle and circle
+# (write three functions to calculate the square, and call them in the main program depending on the user's choice).
+
+def rectangle_square(a, b):
+    return a*b
+
+
+def triangle_square(a, b):
+    return 0.5*a*b
+
+
+def circle_square(a):
+    return 3.14*a**2
