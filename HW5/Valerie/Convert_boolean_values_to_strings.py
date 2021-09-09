b6942cb997db74066ce70dcc072f335fb8b468e2
Valeriia Ievseichyk <valeriia.ievseichyk@gmail.com>
2021-09-09 14:23:19 +0300

    added homework5

---
+++ HW5/Valerie/Convert_boolean_values_to_strings.py
@@ -0,0 +1,6 @@
+def bool_to_word(boolean):
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
+        
---
+++ HW5/Valerie/Counting_sheep.py
@@ -0,0 +1,7 @@
+def count_sheeps(sheep):
+    count = 0
+    for number_of_sheeps in sheep:
+        if number_of_sheeps:
+            count += 1
+    return count
+    
