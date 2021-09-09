a77ff5a5d3f3ca42b37b393123615e5509a17477
grantigor <grant.igor.v@gmail.com>
2021-09-09 17:09:07 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   HW6/Grant_Igor/HW6_Task2.py 	renamed:    HW6/Grant_Igor/squere.py -> HW6/Grant_Igor/square.py

--- HW6/Grant_Igor/square.py
+++ HW6/Grant_Igor/square.py
@@ -1,16 +1,16 @@
 import math
 def triangle(a, b, c):
-    # Function will calculate the squere of the triangle
+    # Function will calculate the square of the triangle
     p = (a+b+c)/2
     s_t = round(math.sqrt(p*(p-a)*(p-b)*(p-c)), 2)
     return s_t
 
 def rectangle(a, b):
-    # Function will calculate the squere of the rectangle
+    # Function will calculate the square of the rectangle
     s_r = round((a*b), 2)
     return s_r
 
 def circle(r):
-    # Function will calculate the squere of the circle
+    # Function will calculate the square of the circle
     s_c = round(math.pi*(r**2), 2)
     return s_c
