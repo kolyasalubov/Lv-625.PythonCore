478ab80139870e7af254417efde23d7dd7ecf64c
grantigor <grant.igor.v@gmail.com>
2021-09-09 16:58:51 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   HW6/Grant_Igor/HW6_Task2.py 	deleted:    HW6/Grant_Igor/sqr_circle.py 	deleted:    HW6/Grant_Igor/sqr_rectangle.py 	deleted:    HW6/Grant_Igor/sqr_triangle.py 	new file:   HW6/Grant_Igor/squere.py

--- HW6/Grant_Igor/HW6_Task2.py
+++ HW6/Grant_Igor/HW6_Task2.py
@@ -1,16 +1,14 @@
-from sqr_rectangle import rectangle as s_r
-from sqr_triangle import triangle as s_t
-from sqr_circle import circle as s_c
+import squere as sq
 figure = int(input("Select the figure for squere calculation: 1. Rectangle, 2. Triangle, 3. Circle "))
 if figure == 1:
     side_rect_a = float(input("Input side a: "))
     side_rect_b = float(input("Input side b: "))
-    print(f"The squere of rectangle is: {s_r(side_rect_a, side_rect_b)}")
+    print(f"The squere of rectangle is: {sq.rectangle(side_rect_a, side_rect_b)}")
 elif figure == 2:
     side_trian_a = float(input("Input side a: "))
     side_trian_b = float(input("Input side b: "))
     side_trian_c = float(input("Input side c: "))
-    print(f"The squere of the triangle is: {s_t(side_trian_a, side_trian_b, side_trian_c)}")
+    print(f"The squere of the triangle is: {sq.triangle(side_trian_a, side_trian_b, side_trian_c)}")
 elif figure == 3:
     rad_cir = float(input("Input circle radius: "))
-    print(f"The squere of the circle is: {s_c(rad_cir)}")
+    print(f"The squere of the circle is: {sq.circle(rad_cir)}")
