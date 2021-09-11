2f5d5d34cc762865ddab1ca5a176c1b0f0eed4f9
Andriian111 <dorosh.andrwork@gmail.com>
2021-09-11 15:42:16 +0300

    Added homework 5: all tasks

---
+++ HW5/Dorosh_A/bool_to_str_Yes_or_No.py
@@ -0,0 +1,10 @@
+bool_q = bool(input("Enter True or False "))
+
+def boolean_t0_string(bool_qi): 
+    if bool_qi == False:
+        return "No" 
+    else:
+        return "Yes" 
+work = boolean_t0_string(bool_q)
+print(work)
+
