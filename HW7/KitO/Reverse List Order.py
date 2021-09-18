ad5c1ed94cb5366b4fb82c8e07e1adc65f2e8223
KitOlga <kit.olga81@gmail.com>
2021-09-19 02:18:24 +0300

    Added HW7 task Reverse List Order

--- HW7/KitO/Reverse List Order.py
+++ HW7/KitO/Reverse List Order.py
@@ -1,11 +1,15 @@
 ##### https://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/train/python
 """
-In this kata you will create a function that takes in a list and returns a list with the reverse order.
-Examples
-reverse_list([1,2,3,4]) == [4,3,2,1]
-reverse_list([3,1,5,4]) == [4,5,1,3]
+In this kata you will create a function that takes in a list 
+and returns a list with the reverse order.
 """
 
 def reverse_list(l):
-  'return a list with the reverse order of l'
-  pass
+    r = l.reverse()
+    return l
+
+list1 = [1,2,3,4]
+list2 = [3,1,5,4]
+
+print(reverse_list(list1))  # [4, 3, 2, 1]
+print(reverse_list(list2))  # [4, 5, 1, 3]
