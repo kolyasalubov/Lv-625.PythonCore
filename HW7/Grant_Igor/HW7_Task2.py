cc94ca335281da635acd954512df9aba3643e3e1
grantigor <grant.igor.v@gmail.com>
2021-09-18 15:31:08 +0300

    On branch Grant_Igor Changes to be committed: 	modified:   HW7/Grant_Igor/HW7_Task2.py

--- HW7/Grant_Igor/HW7_Task2.py
+++ HW7/Grant_Igor/HW7_Task2.py
@@ -1,5 +1,21 @@
 import re
-def password():
-    password_input = input("enter your password")
-    for password_input in range(6-16):
-        if i in re.search("[a-z]", password_input)
+"""
+Password generator for module re
+"""
+def password_gen_two():
+    while True:
+        password = input("Enter password: ")
+        if len(password) < 6 or len(password) > 16:
+            print("Password lenght should be between 6 and 16 symbols")
+        elif not re.search("[a-z]", password):
+            print("Use lower case letter in range 'a-z'")
+        elif not re.search("[A-Z]", password):
+            print("Use upper case letter in range 'A-Z'")
+        elif not re.search("[0-9]", password):
+            print("Use number in range '0-9'")
+        elif not re.search("[@#$%^&*()]", password):
+            print("Use one of the special symbols '@#$%^&*()'")
+        else:
+            break
+    print("Password accepted")
+password_gen_two()
