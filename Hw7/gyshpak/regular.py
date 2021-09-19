cef17332b9a3e92a64773d49aee31c5466cd85b9
Gena Shpak <gy.shpak@gmail.com>
2021-09-19 13:29:08 +0300

    modified:   Hw7/gyshpak/regular.py

--- Hw7/gyshpak/regular.py
+++ Hw7/gyshpak/regular.py
@@ -1,7 +1,33 @@
 import re
-
-my_string = '88888888'
-my_match = re.fullmatch(r'[(a-z)+(A-Z)+(0-9)+]{6,8}', my_string)
-# my_match = re.fullmatch(r'[a-zA-Z0-9]{6,16}', my_string)
-# my_match = re.fullmatch(r'([a-z]+[A-Z]+[0-9]+[$#@]+){6-16}', my_string)
-print('YES' if my_match else 'NO')
+def check_pass(in_pass):
+    count_check = 0
+    my_answer = "You password must contain"
+    if re.fullmatch(r'[a-zA-Z0-9$#@]{6,16}', in_pass):
+        count_check += 1
+        print('1')
+    else:
+        my_answer += " from 6 to 16 simbols "
+    if re.fullmatch(r'.*[a-z]+.*', in_pass):
+        count_check += 1
+        print('2')
+    else:
+        my_answer += ", one or more a-z simbols "
+    if re.fullmatch(r'.*[A-Z]+.*', in_pass):
+        count_check += 1
+        print('3')
+    else:
+        my_answer += ", one or more A-Z simbols "
+    if re.fullmatch(r'.*[0-9]+.*', in_pass):
+        count_check += 1
+        print('4')
+    else:
+        my_answer += ", one or more 0-9 simbols "
+    if re.fullmatch(r'.*[$#@]+.*', in_pass):
+        count_check += 1
+        print('5')
+    else:
+        my_answer += ", one or more @#$ simbols "
+    
+    return ("Your password is correct" if count_check == 5 else my_answer)
+    
+print(check_pass(input('insert password ')))
