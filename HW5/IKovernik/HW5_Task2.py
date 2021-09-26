c88b92ff1eb7212a54a98079bb7f03e65d1fb93f
IKovernik <supervanko@gmail.com>
2021-09-26 15:35:20 +0300

    try again to add HW5T1 and 2

---
+++ HW5/IKovernik/HW5_Task2.py
@@ -0,0 +1,8 @@
+login=""
+
+while login!="First":
+    login=input("Input your login ")
+    if login =="First": break
+    else: print("You type wrong login!")
+
+print("Congradulations! 'First' is right login!" )
