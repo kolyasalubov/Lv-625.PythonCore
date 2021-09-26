6f65d4eba6ff292dc04a03a844c129a15f2fb058
IKovernik <supervanko@gmail.com>
2021-09-26 15:29:23 +0300

    delete hw5t1 and hw5t2

--- HW5/IKovernik/HW5_Task2.py
+++
@@ -1,8 +0,0 @@
-login=""
-
-while login!="First":
-    login=input("Input your login ")
-    if login =="First": break
-    else: print("You type wrong login!")
-
-print("Congradulations! 'First' is right login!" )
