ce25947efafd212be626e226530510b570645a6f
IKovernik <supervanko@gmail.com>
2021-09-27 22:11:06 +0300

    add HW7T2 multiples and grasshoper

---
+++ HW7/IKovernik/HW7_Task2.py
@@ -0,0 +1,18 @@
+import re
+
+def validate():
+    while True:
+        password = input("Enter a password: ")
+        if len(password) < 6 or len(password)>16:
+            print("Make sure your password is 6-16 letters length")
+        elif re.search('[0-9]',password) is None:
+            print("Make sure your password has a number in it")
+        elif re.search('[A-Z]',password) is None or re.search('[a-z]',password) is None: 
+            print("Make sure your password includes small [a-z], and captual [A-Z] letters")
+        elif re.search("['#','@','$']",password) is None: 
+            print("Make sure your password at least 1 character from $@#")
+        else:
+            print("Your password seems fine")
+            break
+
+validate()
---
+++ HW7/IKovernik/multiples.py
@@ -0,0 +1,9 @@
+def summation(num):
+    sum=0
+    for i in range(1,num+1):
+        print(i)
+        sum+=i
+    return(sum)
+
+print (summation(8))
+    
