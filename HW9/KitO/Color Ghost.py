d500153113940bd2b691b15153ae29eabfad2aea
skot <skot@student.ucode.world>
2021-09-28 23:31:52 +0300

    Added HW9 Color Ghost

--- HW9/KitO/Color Ghost.py
+++ HW9/KitO/Color Ghost.py
@@ -2,12 +2,20 @@
 """
 Create a class Ghost
 Ghost objects are instantiated without any arguments.
-Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
 
 ghost = Ghost()
 ghost.color  => "white" or "yellow" or "purple" or "red"
 """
 
 class Ghost(object):
-    pass
+    def __init__(self):
+        import random
+        color_list = ["white", "yellow", "purple", "red"]
+        self.color = random.choice(color_list)
+        print(self.color)
+            
+ghost1 = Ghost()
+ghost2 = Ghost()
+
 
