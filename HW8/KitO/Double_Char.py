923892b46adf9b00cd51e60a77849b08ed8a94bf
skot <skot@student.ucode.world>
2021-09-28 21:17:27 +0300

    Added HW8 Double Char

--- HW8/KitO/Double_Char.py
+++ HW8/KitO/Double_Char.py
@@ -1,15 +1,24 @@
-#####
+##### https://www.codewars.com/kata/double-char/train/python
 """
-According to the creation myths of the Abrahamic religions, 
-Adam and Eve were the first Humans to wander the Earth.
-You have to do God's job. 
-The creation method must return an array of length 2 containing objects 
-(representing Adam and Eve). 
-The first object in the array should be an instance of the class Man. 
-The second should be an instance of the class Woman. 
-Both objects have to be subclasses of Human. 
-Your job is to implement the Human, Man and Woman classes.
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+double_char("String") ==> "SSttrriinngg"
+double_char("Hello World") ==> "HHeelllloo  WWoorrlldd"
+double_char("1234!_ ") ==> "11223344!!__  "
 """
 
+def double_char(s):
+    ss = []
+    for i in range(0,len(s)):
+        ss.append(s[i])
+        ss.append(s[i])
+    return "".join(ss)
+    
+string1 = "String"
+print(double_char(string1))     # SSttrriinngg
 
+string2 = "Hello World"
+print(double_char(string2))     # HHeelllloo  WWoorrlldd
+
+string3 = "1234!_ "
+print(double_char(string3))     # 11223344!!__  
 
