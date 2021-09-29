0bde065f283d25a773eb229c3e0720704f11c527
IKovernik <supervanko@gmail.com>
2021-09-29 20:22:42 +0300

    fixed ball

--- HW9/IKovernik/Ball_superball.py
+++ HW9/IKovernik/Ball_superball.py
@@ -1,26 +1,4 @@
-class Bird:
-
-    def __init__(self):
-        print("Bird is ready")
-
-    def whoisThis(self):
-        print("Bird")
-
-    def swim(self):
-        print("Swim faster")
-
-class Penguin(Bird):
-
-    def __init__(self):
-        super().__init__()
-        print("Penguin is ready")
-    def whoisThis(self):
-        print("Penguin")
-
-    def run(self):
-        print("run faster")
-
-peggy=Penguin()
-peggy.whoisThis()
-peggy.swim()
-peggy.run()
+class Ball(object):
+    
+    def __init__ (self, ball_type="regular"):
+        self.ball_type=ball_type
