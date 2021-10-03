821c01c2e86c35a5f3413bdaf3a60a507c062f4d
Oleh Horbai <oleg.gorbay.02@gmail.com>
2021-10-03 15:06:56 +0300

    added 3 tasks from codewars

---
+++ HW8/Olehbai/Double char.py
@@ -0,0 +1,3 @@
+def double_char(s):
+     return ''.join(i * 2 for i in s)
+     
---
+++ HW8/Olehbai/Summation.py
@@ -0,0 +1,4 @@
+def summation(num):
+    return sum(range(1, num+1))
+numbers = summation(int(input("Enter num: ")))   
+print(numbers) 
